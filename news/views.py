ac7932086cb877ec697f54e7e8d526b164e97657
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2018-04-01 15:52:07 +0530

    Integrate seo tools with django

--- news/views.py
+++ news/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from toolset import *
+
 
 # Create your views here.
 def articles_list(request):
-    return render(request, 'news/articles_list.html', {})
+    nesting_status = get_table_status("http://www.yourhtmlsource.com/tables/nestingtables.html")
+    return render(request, 'news/articles_list.html', {'nesting_status': nesting_status})
