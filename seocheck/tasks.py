363b8034a0e3d1990e2cb6ccc74cfe838f787c95
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2018-04-03 04:37:57 +0530

    Implement running toolset task on celery

--- seocheck/tasks.py
+++ seocheck/tasks.py
@@ -1,7 +1,9 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from toolset import *
 
 @shared_task
 def launch_all(url):
-    return url
+    nesting_status = get_table_status("http://www.yourhtmlsource.com/tables/nestingtables.html")
+    return nesting_status
--- seocheck/views.py
+++ seocheck/views.py
@@ -21,21 +21,13 @@ def get_seocheck(request):
     if request.method == 'POST':
         form = SeocheckForm(request.POST)
         if form.is_valid():
-            print("%%%%%%%%%%%%%%%%%%% HOME %%%%%%%%%%%%%%%%%%%")
             seoUrl = form.cleaned_data['seoUrl']
             task_results = launch_all.delay(seoUrl)
-            print("--------------- traceback ---------------")
-            print(task_results.traceback)
-            time.sleep(1)
             request.session['seocheck_task_id'] = task_results.task_id
-            print(seoUrl)
-            print("Heyyyyyyyyyyyyyyyyyyy down hereeee!")
-            print(task_results.ready())
             # return HttpResponseRedirect('/thanks/')
             return HttpResponseRedirect(route_url('get_seocheck_results', args=[]))
     else:
         form = SeocheckForm()
-    print("%%%%%%%%%%%%%%%%%%% END HOME %%%%%%%%%%%%%%%%%%%")
     return render(request, 'seocheck/home.html', {'form': form})
 
 
@@ -45,8 +37,6 @@ def get_seocheck_results(request):
         return HttpResponseRedirect(route_url('get_seocheck', args=[]))
     seocheck_results = AsyncResult(seocheck_task_id)
     print(seocheck_results)
-    if request.method == 'GET':
-        print("THIS IS GET")
     return render(request, 'seocheck/seocheck.html')
 
 
@@ -57,14 +47,12 @@ def ajax_seocheck_results(request):
     # ==========================================
     seocheck_task_id = request.session.get('seocheck_task_id')
     seocheck_results = AsyncResult(seocheck_task_id)
-    print("%%%%%%%%%%%%%%%%%%% RESULT PAGE %%%%%%%%%%%%%%%%%%%")
-    print(seocheck_results.result)
-    print(seocheck_results.ready())
+    # print(seocheck_results.result)
+    # print(seocheck_results.ready())
     # print(seocheck_results.get(timeout=1))
-    print(seocheck_results.traceback)
+    # print(seocheck_results.traceback)
 
     # ==========================================
     response_data['result'] = 'success'
     response_data['data'] = seocheck_results.result
-    print("%%%%%%%%%%%%%%%%%%% END RESULT PAGE %%%%%%%%%%%%%%%%%%%")
     return JsonResponse(response_data)
