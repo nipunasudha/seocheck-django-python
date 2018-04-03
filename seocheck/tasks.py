cab4be396c70e197d0b6ab21bd242d3e679d6867
Nipuna Sudharaka <nipuna.14@cse.mrt.ac.lk>
2018-04-03 08:16:48 +0530

    Add task group and get no. of tasks completed

--- seocheck/tasks.py
+++ seocheck/tasks.py
@@ -1,9 +1,31 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
+import time
 from celery import shared_task
-from toolset import *
+from pyquery import PyQuery as pq
+
+from toolset import get_table_status
+
 
 @shared_task
 def launch_all(url):
     nesting_status = get_table_status("http://www.yourhtmlsource.com/tables/nestingtables.html")
     return nesting_status
+
+
+@shared_task
+def sub1():
+    time.sleep(2)
+    return "ONE"
+
+
+@shared_task
+def sub2():
+    time.sleep(4)
+    return "TWO"
+
+
+@shared_task
+def sub3():
+    time.sleep(6)
+    return "THREE"
--- seocheck/views.py
+++ seocheck/views.py
@@ -1,29 +1,31 @@
 import time
 
+from celery import group
 from django.shortcuts import render
-from toolset import *
 from django.http import HttpResponseRedirect
 from .forms import SeocheckForm
 from django.core.urlresolvers import reverse as route_url
-from celery.result import AsyncResult
+from celery.result import AsyncResult, GroupResult
 import json
 from django.http import JsonResponse
 from django.http import HttpResponse
-from .tasks import launch_all
+from .tasks import sub1, sub2, sub3
 
-# Create your views here.
-def articles_list(request):
-    nesting_status = get_table_status("http://www.yourhtmlsource.com/tables/nestingtables.html")
-    return render(request, 'seocheck/home.html', {'nesting_status': nesting_status})
+global_job_list = []
 
 
 def get_seocheck(request):
+    global global_job_list
     if request.method == 'POST':
         form = SeocheckForm(request.POST)
         if form.is_valid():
             seoUrl = form.cleaned_data['seoUrl']
-            task_results = launch_all.delay(seoUrl)
-            request.session['seocheck_task_id'] = task_results.task_id
+            # task_results = launch_all.delay(seoUrl)
+            job = group([sub1.s(), sub2.s(), sub3.s()])
+            task_results = job.apply_async()
+            request.session['seocheck_task_index'] = len(global_job_list)
+            global_job_list.append(task_results)
+            request.session['seocheck_url'] = seoUrl
             # return HttpResponseRedirect('/thanks/')
             return HttpResponseRedirect(route_url('get_seocheck_results', args=[]))
     else:
@@ -35,24 +37,26 @@ def get_seocheck_results(request):
     seocheck_task_id = request.session.get('seocheck_task_id')
     if not seocheck_task_id:
         return HttpResponseRedirect(route_url('get_seocheck', args=[]))
-    seocheck_results = AsyncResult(seocheck_task_id)
-    print(seocheck_results)
     return render(request, 'seocheck/seocheck.html')
 
 
 def ajax_seocheck_results(request):
+    global global_job_list
     print("AJAX")
     # return {'a': 'b'}
     response_data = {}
     # ==========================================
-    seocheck_task_id = request.session.get('seocheck_task_id')
-    seocheck_results = AsyncResult(seocheck_task_id)
+    seocheck_task_index = int(request.session.get('seocheck_task_index'))
+    seocheck_url = request.session.get('seocheck_url')
+    seocheck_results = global_job_list[seocheck_task_index]
+    print(seocheck_results.completed_count())
     # print(seocheck_results.result)
     # print(seocheck_results.ready())
     # print(seocheck_results.get(timeout=1))
     # print(seocheck_results.traceback)
 
     # ==========================================
-    response_data['result'] = 'success'
-    response_data['data'] = seocheck_results.result
+    response_data['status'] = 'success'
+    response_data['url'] = seocheck_url
+    response_data['result'] = seocheck_results.completed_count()
     return JsonResponse(response_data)
